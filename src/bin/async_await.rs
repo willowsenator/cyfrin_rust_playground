102faafc19d4abc5003db6d854ada9339c0d9c66
willowsenator <willowsenator@gmail.com>
2025-07-20 21:16:35 +0200

    using await with tokio

---
+++ src/bin/async_await.rs
@@ -0,0 +1,17 @@
+use std::time::Duration;
+use tokio::time::sleep;
+
+async fn hello() {
+    println!("Hello async Rust!");
+}
+
+async fn add(x: u32, y: u32) -> u32 {
+    let _ = sleep(Duration::from_millis(1000));
+    x + y
+}
+#[tokio::main]
+async fn main() {
+    hello().await;        
+    let sum = add(1, 2).await;
+    println!("sum: {sum}");
+}
