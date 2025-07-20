b40514d8ff017fbb1e9dbfca65e3f1eae944787f
willowsenator <willowsenator@gmail.com>
2025-07-20 17:11:15 +0200

    lifetime in rust

---
+++ src/bin/lifetime.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+struct Config<'a> {
+    url: &'a str,
+    port: u32,
+    db_url: &'a str,
+}
+
+// Lifetime 'a can be omitted. Rust can automatically figure out the lifetime
+fn log(config: &Config) {
+    println!("Connecting to database at {}...", config.db_url);
+    println!("server listening at {}:{}...", config.url, config.port);
+}
+
+fn main() {
+    let config = Config {
+        url: "localhost",
+        port: 3000,
+        db_url: "db://localhost",
+    };
+
+    log(&config);
+}
