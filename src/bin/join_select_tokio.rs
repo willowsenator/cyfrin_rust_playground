b75a0197df103a8ade945161fcc0a9b0305384e5
willowsenator <willowsenator@gmail.com>
2025-07-20 21:43:37 +0200

    join and select with tokio

---
+++ src/bin/join_select_tokio.rs
@@ -0,0 +1,42 @@
+use std::time::Duration;
+use tokio::time::sleep;
+use tokio::{join, select};
+
+async fn get_eth_price_from_exchange_1() -> u32 {
+    sleep(Duration::from_millis(1000)).await;
+    1000
+}
+
+async fn get_eth_price_from_exchange_2() -> u32 {
+    sleep(Duration::from_millis(1000)).await;
+    1010
+}
+
+async fn get_btc_price_from_exchange_1() -> u32 {
+    sleep(Duration::from_millis(500)).await;
+    20000
+}
+
+#[tokio::main]
+async fn main() {
+    // Exercise 1
+    let (eth_price, btc_price) = join!(
+        get_eth_price_from_exchange_1(),
+        get_btc_price_from_exchange_1()
+    );
+
+    
+    println!("join: ETH price: {}", eth_price);
+    println!("join: BTC price: {}", btc_price);
+
+    // Exercise 2
+    let eth_price: u32 = select! {
+        price = get_eth_price_from_exchange_1() => {
+            price
+        }
+        price = get_eth_price_from_exchange_2() => {
+            price
+        }
+    };
+    println!("select: ETH price: {}", eth_price);
+}
